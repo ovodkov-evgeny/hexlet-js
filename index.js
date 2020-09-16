e97b641b252210679c68848a611be2030bf837a7
Evgeny Ovodkov <kefir4ek@gmail.com>
2020-09-16 11:26:34 +0300

    settings

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import _ from "lodash";
 
-// console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
 
-console.log(_.capitalize("hello"));
+// console.log(_.capitalize("hello"));
+let getNum = (arr) => {
+  for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]);
+  }
+};
